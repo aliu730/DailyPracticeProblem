dd3b41275376e16934658333b5a931771d91c4cd
Andrew Liu <aliu730@gmail.com>
2018-09-03 18:53:10 -0700

    romanToInteger done all test pass good stats

---
+++ romanToInteger.js
@@ -0,0 +1,24 @@
+var romanToInt = function(s) {
+  var dict = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+  };
+  var currTotal = dict[s[0]]; //1000
+  var prev = dict[s[0]];  //1000
+  for (var i = 1; i < s.length; i++) {
+    if (dict[s[i]] > prev) {
+      currTotal -= prev;
+      currTotal += (dict[s[i]] - prev);
+      prev = dict[s[i]];
+    } else {
+      currTotal += dict[s[i]];
+      prev = dict[s[i]];
+    }
+  }
+  return currTotal;
+};
